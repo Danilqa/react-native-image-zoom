f25261acc70b20d587a717794c30e96f14bf75f0
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-06-11 22:01:42 +0300

    common: added moving calculator

---
+++ jest.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    clearMocks: true,
+    testMatch: [
+        '<rootDir>/src/**/*.spec.(ts|tsx)'
+    ],
+    // This configuration is used to defeat the problem:
+    //  jest-haste-map: @providesModule naming collision:
+    //   Duplicate module name: core-ui-kit
+    //   Paths: C:\dev\projects\ams\core-ui-kit\dist\package.json collides with
+    //      C:\dev\projects\ams\core-ui-kit\package.json
+    //  This warning is caused by a @providesModule declaration with the same name across two different files.
+    modulePathIgnorePatterns: [
+        '<rootDir>/built/'
+    ],
+    testURL: 'http://localhost',
+    transform: {
+        '^(?!.*\\.(js|ts|tsx|css|json)$)': '<rootDir>/config/jest/transform/file.transform.js',
+        '^.+\\.css$': '<rootDir>/config/jest/transform/css.transform.js',
+        '^.+\\.tsx?$': '<rootDir>/config/jest/transform/typescript.transform.js'
+    },
+    moduleFileExtensions: [
+        'web.ts',
+        'ts',
+        'tsx',
+        'web.js',
+        'js',
+        'json',
+        'node'
+    ],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.spec.json'
+        }
+    },
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    }
+};
